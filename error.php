afaf49340597163f767e6a585ee4ef3f34738460
danjeffrey <dan@apexvis.com>
2014-04-30 13:20:37 -0500

    First commit

---
+++ error.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+$title = "Dan Jeffrey";
+include '../header.php';
+?>
+
+<h1>
+    <span>Missing URL</span>
+</h1>
+<div class="info-container">
+    <div class="inner-border clear-fix">
+        <h2 class="info-top">Page Not Found Error (HTML 404 Error)</h2>
+        <div>
+            <h3>If you're a <strong>visitor</strong> and not sure what happened:</h3>
+            <ol>
+                <li>
+                    1) You entered or copied the URL incorrectly or
+                </li>
+                <li>
+                    2) The link you used to get here is faulty.
+                </li>
+                <li>
+                    3) Dan Jeffrey provided an incorrect URL. Please contact me.
+                </li>                
+            </ol>
+            <p>Please return to the top level page.</p> 
+        </div>
+    </div>
+</div>
+
+
+<!-- end div, class = main -->
+<?php
+include '../footer.php';
+?>
